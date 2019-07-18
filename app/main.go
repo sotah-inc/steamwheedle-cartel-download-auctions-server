4c9bf21ec94e0e3ca6f48cc8a24f0d64e8a7e04a
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-18 16:08:46 -0700

    Add better logging to download-auctions-server service.

--- app/main.go
+++ app/main.go
@@ -7,12 +7,16 @@ import (
 	"os"
 	"strconv"
 
+	"cloud.google.com/go/compute/metadata"
 	"github.com/gorilla/mux"
-
+	"github.com/sirupsen/logrus"
 	"github.com/sotah-inc/steamwheedle-cartel/pkg/logging"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/logging/stackdriver"
 )
 
 var port int
+var serviceName string
+var projectId string
 
 func init() {
 	parsedPort, err := strconv.Atoi(os.Getenv("PORT"))
@@ -23,13 +27,44 @@ func init() {
 	}
 
 	port = parsedPort
+	serviceName = os.Getenv("K_SERVICE")
+	projectId, err = metadata.Get("project/project-id")
+	if err != nil {
+		log.Fatalf("Failed to get port: %s", err.Error())
+
+		return
+	}
 }
 
 func main() {
+	// establishing log verbosity
+	logVerbosity, err := logrus.ParseLevel("info")
+	if err != nil {
+		logging.WithField("error", err.Error()).Fatal("Could not parse log level")
+
+		return
+	}
+	logging.SetLevel(logVerbosity)
+
+	// adding stackdriver hook
+	logging.WithField("project-id", projectId).Info("Creating stackdriver hook")
+	stackdriverHook, err := stackdriver.NewHook(projectId, serviceName)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error":     err.Error(),
+			"projectID": projectId,
+		}).Fatal("Could not create new stackdriver logrus hook")
+
+		return
+	}
+	logging.AddHook(stackdriverHook)
+
+	logging.WithField("service", serviceName).Info("Initializing service")
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := fmt.Fprint(w, "Hello, world!!!"); err != nil {
-			logging.WithField("error", err.Error()).Error("")
+			logging.WithField("error", err.Error()).Error("Failed to return response")
 
 			return
 		}
